85e721fef4d0b9548eefe09ccf17394db29497d1
Fevzierenn <fevzierencicek@gmail.com>
2024-01-05 00:13:34 +0300

    hocalara ve sınıflara ders atama işlemi gerçekleşti. Aynı zamanda da çakışma durumuna karşı gelindi

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # DersProgrami-Backend
 
 #### React ile Backend kısmı birleştirildi.
+
+##### Hocaya ders atama işlemleri yapıldı.
+
+##### Sınıfa ders atama işlemleri yapıldı.
+
+##### Kesişen ders ya da sınıfın boş olmaması durumunda ders ataması yapılamamaktadır.
--- src/main/java/DersProgrami/DersProgramiYazGel1/rest/Controller.java
+++ src/main/java/DersProgrami/DersProgramiYazGel1/rest/Controller.java
@@ -1,14 +1,17 @@
 package DersProgrami.DersProgramiYazGel1.rest;
 
 import DersProgrami.DersProgramiYazGel1.entities.Ders;
+import DersProgrami.DersProgramiYazGel1.entities.DersProgrami;
 import DersProgrami.DersProgramiYazGel1.entities.Hoca;
 import DersProgrami.DersProgramiYazGel1.entities.Sinif;
+import DersProgrami.DersProgramiYazGel1.services.DersProgramiService;
 import DersProgrami.DersProgramiYazGel1.services.DersService;
 import DersProgrami.DersProgramiYazGel1.services.HocaService;
 import DersProgrami.DersProgramiYazGel1.services.SinifService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Time;
 import java.util.List;
 @CrossOrigin()
 @RestController
@@ -21,6 +24,8 @@ public class Controller {
     private HocaService hocaService;
     @Autowired
     private SinifService sinifService;
+    @Autowired
+    private DersProgramiService dersProgramiService;
 
     @GetMapping
     private String sayHello(){
@@ -46,5 +51,20 @@ public class Controller {
         return sinifService.sinifEkle(sinif);
     }
 
+
+    @PostMapping("/derskoy")
+    private String dersProgramiEkle(@RequestBody DersProgrami dersProgrami){
+        return dersProgramiService.dersPrograminaEkle(dersProgrami);
+    }
+
+
+//    @PostMapping("/derskoy/{dersid}/{hocaid}/{gunid}/{sinifid}/{baslangic}/{bitis}")
+//    private void dersPrograminaDersKoy(@PathVariable int dersid, @PathVariable int hocaid,
+//                                       @PathVariable int gunid, @PathVariable String sinifid,
+//                                       @PathVariable Time baslangic, @PathVariable Time bitis
+//                                       ){
+//        System.out.println("dersid:"+dersid+ " hocaid:"+ hocaid);
+//        dersProgramiService.dersPrograminaDersEkle(dersid,hocaid,gunid,sinifid,baslangic,bitis);
+//    }
 
 }
