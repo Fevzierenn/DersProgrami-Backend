af41fd227a3022913df415e04ad9fae085b06ff3
Fevzierenn <fevzierencicek@gmail.com>
2024-01-07 13:51:18 +0300

    Ders tablosu görüntülendi, ilgili dersler silinebiliyor.

---
+++ src/main/java/DersProgrami/DersProgramiYazGel1/entities/TabloDegerleri.java
@@ -0,0 +1,14 @@
+package DersProgrami.DersProgramiYazGel1.entities;
+
+import java.time.LocalTime;
+
+public class TabloDegerleri {
+    int program_id;
+    String dersIsmi;
+    String hocaIsmi;
+    String hocaSoyIsmi;
+    String sinif_id;
+    String gun;
+    LocalTime baslangic;
+    LocalTime bitis;
+}
